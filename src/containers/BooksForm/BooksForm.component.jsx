fbdd83a5bc447d80e796654c0c54ee9854464f75
Moin Khan <kmoin786@gmail.com>
2021-02-05 22:52:16 +0530

    Make the BooksForm component stateful

--- src/containers/BooksForm/BooksForm.component.jsx
+++ src/containers/BooksForm/BooksForm.component.jsx
@@ -1,22 +1,28 @@
+import { useState } from 'react';
+
 const BOOK_CATEGORY = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <form id="book-form" action="#">
-    <input type="text" name="title" id="title" />
-    <select name="category" id="category">
-      <option value="Default">Default</option>
-      {
-        BOOK_CATEGORY.map(bookCategory => (
-          <option
-            key={BOOK_CATEGORY.indexOf(bookCategory)}
-            value={bookCategory}
-          >
-            {bookCategory}
-          </option>
-        ))
-      }
-    </select>
-  </form>
-);
+const BooksForm = () => {
+  const [input, handleChange] = useState({ text: '', category: 'Default' });
+
+  return (
+    <form id="book-form" action="#">
+      <input type="text" name="title" id="title" onChange={e => handleChange({ ...input, text: e.target.value })} />
+      <select name="category" id="category" onChange={e => handleChange({ ...input, category: e.target.value })}>
+        <option value="Default">Default</option>
+        {
+          BOOK_CATEGORY.map(bookCategory => (
+            <option
+              key={BOOK_CATEGORY.indexOf(bookCategory)}
+              value={bookCategory}
+            >
+              {bookCategory}
+            </option>
+          ))
+        }
+      </select>
+    </form>
+  );
+};
 
 export default BooksForm;
