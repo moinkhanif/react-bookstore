9a9e976c0d542240bee31a35b5c6aa9d6ffec6b4
Moin Khan <kmoin786@gmail.com>
2021-02-05 20:21:16 +0530

    Move BookForm to containers folder

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,6 +1,6 @@
 import './App.styles.css';
 import BooksList from '../BooksList/BooksList.component';
-import BooksForm from '../BooksForm/BooksForm.component';
+import BooksForm from '../../containers/BooksForm/BooksForm.component';
 
 function App() {
   return (
