88257951fe4156505913286b8ae5930e012f3db9
Moin Khan <kmoin786@gmail.com>
2021-02-05 09:43:59 +0530

    Initialize Redux Store and connect required component to state

--- src/components/BooksList/BooksList.component.jsx
+++ src/components/BooksList/BooksList.component.jsx
@@ -1,11 +1,36 @@
-const BookList = () => (
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Book from '../Book/Book.component';
+
+const BookList = ({ books }) => (
   <table>
     <tr>
       <th>Book Id</th>
       <th>Title</th>
       <th>Category</th>
     </tr>
+    {books.map(book => <Book book={book} key={book.id} />)}
   </table>
 );
 
-export default BookList;
+BookList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    category: PropTypes.string,
+  })),
+};
+
+BookList.defaultProps = {
+  books: {
+    id: 'Undefined',
+    title: 'Undefined',
+    category: 'Undefined',
+  },
+};
+
+const mapStateToProps = state => ({
+  books: state.books,
+});
+
+export default connect(mapStateToProps)(BookList);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
 import App from './components/App/App.component';
 import reportWebVitals from './reportWebVitals';
 
+// eslint-disable-next-line no-unused-vars
+const initialState = [
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Eloquent JavaScript, Second Edition',
+    category: 'Learning',
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'The Great Gatsby',
+    category: 'Learning',
+  },
+];
+
+const store = createStore(rootReducer, initialState);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
