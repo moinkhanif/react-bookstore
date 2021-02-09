fcfd8d9203c0764967b4ee37c28658fbe35b5044
Moin Khan <kmoin786@gmail.com>
2021-02-09 19:30:05 +0530

    Style BooksForm and its children

--- src/containers/BooksForm/BooksForm.component.jsx
+++ src/containers/BooksForm/BooksForm.component.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { CREATE_BOOK } from '../../actions/index';
+import './BooksForm.styles.css';
 
 export const BOOK_CATEGORY = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
 const BooksForm = () => {
-  const initialInput = { text: '', category: 'Default', valid: true };
+  const initialInput = {
+    text: '', category: 'Default', valid: true, errorId: 0, errorMessage: '',
+  };
   const [input, handleChange] = useState(initialInput);
   const dispatch = useDispatch();
 
   const handleSubmit = event => {
     event.preventDefault();
     if (input.text.length < 2) {
-      handleChange({ ...input, text: '', valid: false });
+      handleChange({
+        ...input, text: '', errorId: 1, valid: false, errorMessage: 'Enter Valid Book Name',
+      });
+    } else if (input.category === 'Default') {
+      handleChange({
+        ...input, errorId: 2, valid: false, errorMessage: 'Category cannot be Default value',
+      });
     } else {
       dispatch(CREATE_BOOK({
         id: Math.floor(Math.random() * 1000),
@@ -24,33 +33,41 @@ const BooksForm = () => {
   };
 
   return (
-    <form id="book-form" action="#">
-      <input
-        type="text"
-        name="title"
-        id="title"
-        onChange={e => handleChange({ ...input, text: e.target.value })}
-        minLength="2"
-        placeholder={input.valid ? 'Enter Book Name' : 'Enter Valid Book Name'}
-        className={input.valid ? 'valid' : 'invalid'}
-        value={input.text}
-        required
-      />
-      <select name="category" id="category" onChange={e => handleChange({ ...input, category: e.target.value })} value={input.category}>
-        <option value="Default">Default</option>
-        {
-          BOOK_CATEGORY.map(bookCategory => (
-            <option
-              key={BOOK_CATEGORY.indexOf(bookCategory)}
-              value={bookCategory}
-            >
-              {bookCategory}
-            </option>
-          ))
-        }
-      </select>
-      <input type="submit" onClick={handleSubmit} />
-    </form>
+    <div className="max-width-limit">
+      <h2 className="form-title">Add New Book</h2>
+      <form id="book-form" className="max-width-limit" action="#">
+        <div className="form-content">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            onChange={e => handleChange({ ...input, text: e.target.value })}
+            minLength="2"
+            placeholder={input.valid ? 'Enter Book Name' : 'Enter Valid Book Name'}
+            className={input.valid && input.errorId === 1 ? 'valid' : 'invalid'}
+            value={input.text}
+            required
+          />
+          <select name="category" id="category" className={input.errorId === 2 && !input.valid ? 'invalid' : ''} onChange={e => handleChange({ ...input, category: e.target.value })} value={input.category}>
+            <option value="Default">Default</option>
+            {
+              BOOK_CATEGORY.map(bookCategory => (
+                <option
+                  key={BOOK_CATEGORY.indexOf(bookCategory)}
+                  value={bookCategory}
+                >
+                  {bookCategory}
+                </option>
+              ))
+            }
+          </select>
+          <input type="submit" onClick={handleSubmit} value="ADD BOOK" />
+        </div>
+        <div className="error-message">
+          <p>{`Error: ${input.errorMessage}`}</p>
+        </div>
+      </form>
+    </div>
   );
 };
 
